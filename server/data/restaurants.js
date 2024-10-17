670766fd78b2cead1f8a2844913b39a1f421f8bc
uhhitsnathan <nathans2003@icloud.com>
2024-10-16 22:35:59 -0700

    copied all code from project 2

--- server/data/restaurants.js
+++ server/data/restaurants.js
@@ -1,4 +1,61 @@
-// Fill this in
+// restaurants.js
+export const restaurants = [
+    {
+        name: "Mom Can Cook Thai Kitchen",
+        phone: "(661) 251-8103",
+        address: "18358 Soledad Canyon Rd, Santa Clarita, CA 91387",
+        photo: "/images/download.jpeg"
+    },
+    {
+        name: "Toppers Pizza",
+        phone: "(661) 222-7888",
+        address: "8417 Soledad Canyon Rd, Santa Clarita, CA 91387",
+        photo: "/images/toppers.jpeg"
+    },
+    {
+        name: "Gyromania",
+        phone: "(661) 252-4976",
+        address: "20655 Soledad Canyon Rd, Santa Clarita, CA 91351",
+        photo: "/images/gyro.jpeg"
+    },
+    {
+        name: "The Stand",
+        phone: "(661) 339-2333",
+        address: "24201 Valencia Blvd Suite 3260, Valencia, CA 91355",
+        photo: "/images/stand.jpeg"
+    },
+    {
+        name: "Charcoal Korean BBQ",
+        phone: "(661) 251-9292",
+        address: "19158 Soledad Canyon Rd, Canyon Country, CA 91351",
+        photo: "/images/charcoal2.jpg"
+    },
+    {
+        name: "Marston's Restaurant",
+        phone: "(661) 253-9910",
+        address: "24011 Newhall Ranch Rd, Valencia, CA 91355",
+        photo: "/images/martsons3.jpeg"
+    },
+    {
+        name: "Chi-Chi's Pizza",
+        phone: "(661) 252-4405",
+        address: "27117 Sierra Hwy, Canyon Country, CA 91351",
+        photo: "/images/chi.jpeg"
+    },
+    {
+        name: "IN-N-Out Burger",
+        phone: "(800) 786-1000",
+        address: "28368 Sand Canyon Rd, Santa Clarita, CA 91351",
+        photo: "/images/out3.jpeg"
+    },
+    {
+        name: "Bonsai Sushi Garden",
+        phone: "(661) 251-9008",
+        address: "19358 Soledad Canyon Rd, Santa Clarita, CA 91351",
+        photo: "/images/bonsai.jpg"
+    }
+];
+
 let restaurantData = [
 
 ];
--- server/server.js
+++ server/server.js
@@ -1,1 +1,39 @@
-// Add your server code here.
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { restaurants } from './data/restaurants.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/attractions', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'attractions.html'));
+});
+
+//dynamic stuff for the new pages
+app.get('/restaurants', (req, res) => {
+  res.render('restaurants', { restaurants });
+});
+
+app.get('/new-restaurant', (req, res) => {
+  res.render('new-restaurant');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
